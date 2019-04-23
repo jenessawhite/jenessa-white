11bf24c45241ee0acd696bbfd276ad2188fe8ce0
Jenessa White <jenessaw31@gmail.com>
2019-04-23 08:44:51 -0500

    reformat header

--- src/components/header.js
+++ src/components/header.js
@@ -3,44 +3,14 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle, menuItems }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: `flex`,
-        justifyContent: `space-between`
-      }}
-    >
-      <div style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <header>
+    <nav>
+      {menuItems.map(link =>
+        <Link key={link.name} to={link.link}>
+          {link.name}
         </Link>
-      </div>
-
-      <div>
-        {menuItems.map(link =>
-          <Link
-            key={link.name}
-            to={link.link}
-            style={{ color: `white`, textDecoration: `none`, margin: `0 5px` }}>
-            {link.name}
-          </Link>
-        )}
-      </div>
-    </div>
+      )}
+    </nav>
   </header>
 )
 
