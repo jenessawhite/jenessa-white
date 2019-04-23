e4ae95c897738a9d923255385b621242a183444c
Jenessa White <jenessaw31@gmail.com>
2019-04-23 08:45:01 -0500

    overwrite styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "../styles/index.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -28,24 +29,17 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <div className="site-wrapper">
         <Header siteTitle={data.site.siteMetadata.title} menuItems={data.site.siteMetadata.menuItems} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+        <main className="site-content">
+          {children}
+        </main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
     )}
   />
 )
