dd647f45f02496ca905ee8aa8930c15eff36dfd8
Jenessa White <jenessaw31@gmail.com>
2019-08-16 23:19:24 -0500

    add social media links

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,25 @@
+import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Header = ({ siteTitle, menuItems }) => (
+const Header = ({ siteTitle, menuItems, socialLinks }) => (
   <header>
+    <div className="brand">
+      <Link to="/">J. White</Link>
+    </div>
     <nav>
-      {menuItems.map(link =>
-        <Link key={link.name} to={link.link}>
-          {link.name}
+      {menuItems.map(page => (
+        <Link key={page.name} to={page.link}>
+          {page.name}
         </Link>
-      )}
+      ))}
+      {socialLinks.map(social => (
+          <a key={social.name} href={social.link} target="_blank" rel="noopener noreferrer" className="social-link">
+            <FontAwesomeIcon icon={['fab', social.icon]} color="#fdfdfd" />
+          </a>
+      ))}
+
     </nav>
   </header>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,10 +8,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import Header from "./header"
 import "./layout.css"
 import "../styles/index.css"
+
+library.add(fab);
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,13 +28,18 @@ const Layout = ({ children }) => (
               name
               link
             }
+            socialLinks {
+              name
+              link
+              icon
+            }
           }
         }
       }
     `}
     render={data => (
-      <div className="site-wrapper">
-        <Header siteTitle={data.site.siteMetadata.title} menuItems={data.site.siteMetadata.menuItems} />
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} menuItems={data.site.siteMetadata.menuItems} socialLinks={data.site.siteMetadata.socialLinks} />
         <main className="site-content">
           {children}
         </main>
@@ -39,7 +48,7 @@ const Layout = ({ children }) => (
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </>
     )}
   />
 )
