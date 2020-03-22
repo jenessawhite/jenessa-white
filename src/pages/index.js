c31f27a72fe85ed76f0f4c2a7bc9e8f3c7763384
Jenessa White <jenessaw31@gmail.com>
2020-03-21 23:47:48 -0500

    remove hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const heroImage = require('../images/jw-back-small.jpeg')
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`Jenessa White`, `developer`, `front-end`, `front-end developer`, `Twin Cities`]} />
 
     <div className="home-hero"
-      style={{ backgroundImage: "linear-gradient(rgba(0,128,128,.7),rgba(255,245,245,.4)),url("+heroImage+")" }}>
+      style={{ backgroundImage: "linear-gradient(rgba(0,128,128,.7),rgba(255,245,245,.4)),url('www.google.com')" }}>
       <div>
         <h1>Hi people, I'm Jenessa White</h1>
         <p>I'm a Front-End Developer in the Twin Cities</p>
