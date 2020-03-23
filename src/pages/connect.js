2a01ba7d22b5779d49f0b7dc0e13a24dfa242282
Jenessa White <jenessaw31@gmail.com>
2020-03-23 13:52:36 -0500

    update connect page content

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -26,26 +26,40 @@ const ConnectPage = () => (
         <Layout>
           <SEO title="Contact" />
           <div className="content-wrapper">
-            <h1>Let{`'`}s Connect!</h1> 
-            <badge>Looking for work!</badge>
-            <p>
-              I am open to talking about tons of things! I'd love to discuss attending trainings, confrences, workshops, speaking gigs, podcasts, etc. <strong>However I'm not open to recruiters, so if you are one, please don't reach out.</strong>
-            </p>
-            <p>
-              So, if you would like to talk about any of the above feel free to <a href="https://jenessawhite.typeform.com/to/LocBUf" target="_blank" rel="noopener noreferrer">fill out this form</a>. I’ll get back to you as soon as I can!
-            <br />
-              <strong>Of course if you want to hit me up on the interwebs via social media you can do that on these:</strong>
-            </p>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-8">
+                  <h1 className="mb-0">Let{`'`}s Connect!</h1>
+                  <div className="border-blue mr-auto"></div>
+                  <badge className="shadow">Looking for work!</badge>
+                </div>
+                <div className="col-md-3"></div>
+              </div>
 
-            <div>
-              {data.site.siteMetadata.socialLinks.map( node => (
-                <a key={node.name} href={node.link} target="_blank" rel="noopener noreferrer" className="social-link">
-                  <FontAwesomeIcon icon={['fab', node.icon]} color="#fdfdfd" />
-                </a>
-              ))}
-            </div>
-            <div style={{ height:'700px', display:'block',position:'relative'}}>
-              <ReactTypeformEmbed url="https://jenessawhite.typeform.com/to/LocBUf" popup={false}/>
+              <div className="row mt-4">
+                <div className="col-12">
+                  <p>
+                    I am open to talking about tons of things! I'd love to discuss attending trainings, confrences, workshops, speaking gigs, podcasts, etc. <strong>However I'm not open to recruiters, so if you are one, please don't reach out.</strong>
+                  </p>
+                  <p>
+                    So, if you would like to talk about any of the above feel free to fill out the form below. I’ll get back to you as soon as I can!
+                    <br />
+                    <strong>Of course if you want to hit me up on the interwebs via social media you can do that on these:</strong>
+                  </p>
+
+                  <div>
+                    {data.site.siteMetadata.socialLinks.map(node => (
+                      <a key={node.name} href={node.link} target="_blank" rel="noopener noreferrer" className="social-link">
+                        <FontAwesomeIcon icon={['fab', node.icon]} color="#fdfdfd" />
+                      </a>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="col-12 position-relative d-block" style={{ height: '700px' }}>
+                  <ReactTypeformEmbed url="https://jenessawhite.typeform.com/to/LocBUf" popup={false} />
+                </div>
+              </div>
             </div>
           </div>
         </Layout>
